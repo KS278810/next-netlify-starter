1541584bc7cafc395ccac2eee46658188e103516
GlassesBear <55037643+KS278810@users.noreply.github.com>
2022-01-08 18:11:06 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="スマホを動かすとここにジャイロ情報が表示されるイメージ" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
